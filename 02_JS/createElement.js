8c755acb22c1fc3ab404d9637dbfa4840b192f6d
Naimur Rahman <55896761+NaimurRahman3954@users.noreply.github.com>
2022-07-07 02:26:20 +0600

    commit

---
+++ 02_JS/createElement.js
@@ -0,0 +1,4 @@
+const article = document.getElementById("first-article");
+const newParagraph = document.createElement("p");
+newParagraph.innerHTML = "This is added by Javasscript.";
+article.appendChild(newParagraph);
